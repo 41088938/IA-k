72b872f9bb44024f1c0ef2051675ae247380c1ca
tattattat34 <162422438+tattattat34@users.noreply.github.com>
2025-04-08 16:55:24 +0800

    Update StaticObjOrVar.cs

--- Assets/_TESTING/Script/StaticObjOrVar.cs
+++ Assets/_TESTING/Script/StaticObjOrVar.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class StaticObjOrVar : MonoBehaviour
 {
     Canvas[] GameUICanvas = new Canvas[3];
-    public Canvas[] NewGameUI = new Canvas[4];
+    public Canvas[] NewGameUI = new Canvas[7];
     int pageNum = 1;
     // Start is called before the first frame update
     void Start()
@@ -15,6 +15,12 @@ public class StaticObjOrVar : MonoBehaviour
         GameUICanvas[2] = GameObject.Find("AskingBar").GetComponent<Canvas>();
         NewGameUI[0] = GameObject.Find("GameCommonUINew").GetComponent<Canvas>();//Main
         NewGameUI[1] = GameObject.Find("Procedure1Canvas").GetComponent<Canvas>();//Procedure1
+        NewGameUI[2] = GameObject.Find("Procedure2Canvas").GetComponent<Canvas>();//Procedure2
+        NewGameUI[3] = GameObject.Find("Procedure3Canvas").GetComponent<Canvas>();//Procedure3
+        NewGameUI[4] = GameObject.Find("Procedure4Canvas").GetComponent<Canvas>();//Procedure4
+        NewGameUI[5] = GameObject.Find("Procedure5Canvas").GetComponent<Canvas>();//Procedure5,For wrong label/AWB/DGD
+        NewGameUI[6] = GameObject.Find("CheckList").GetComponent<Canvas>();//checkList for each item after checking?
+        //NewGameUI[7] = GameObject.Find("FinishLevel").GetComponent<Canvas>();//Finish a level
     }
     public void NextPage()
     {
