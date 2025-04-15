9526c38320d87830e80c14aec86e4b9e3c954e13
ydfigi <c20d62e2ab@gmail.com>
2025-04-15 15:38:00 +0800

    .

---
+++ Assets/ResultCheckList/ResultCheckListPanel.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ResultCheckListPanel : MonoBehaviour, IPointerClickHandler
+{
+    public void OnPointerClick(PointerEventData _eventData)
+    {
+        //ResultCheckList.GetResultCheckList().HideCheckList();
+    }
+}
