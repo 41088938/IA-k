5225a0636d8a6ae08561f9bdb8ca49f5944a9473
ydfigi <c20d62e2ab@gmail.com>
2025-04-16 02:54:22 +0800

    .

--- Assets/_TESTING/Script/FillInComponent.cs
+++ Assets/_TESTING/Script/FillInComponent.cs
@@ -7,13 +7,14 @@ public class FillInComponent : MonoBehaviour
     [ContextMenu("Add Dry ice")]
     public void AddDryIce()
     {
-        DBofBox temp = gameObject.AddComponent<DBofBox>();
+        DBofBox temp = GetComponent<DBofBox>();
+        if (temp == null) temp = gameObject.AddComponent<DBofBox>();
         temp.AWBoption = new string[]
         {
             "Missing the \"Net Quantity weight of Dry Ice\"",
             "Incorrext Hazard Label",
             "Incorrect placement of the \"shipper and consignee Mark\"",
-            "Missing the \"Proper Shipping Name\"",
+            "Missing the \"Proper Shipping Name\" in AWB",
             "Missing the word\"UN\"",
             "Incorrect \"UN number\""
         };
