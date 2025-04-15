91e12c8de14d8cf5aefbe7f59bc62cb75f0974c8
ydfigi <c20d62e2ab@gmail.com>
2025-04-16 02:33:14 +0800

    .

---
+++ Assets/_TESTING/Script/FillInComponent.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FillInComponent : MonoBehaviour
+{
+    [ContextMenu("Add Dry ice")]
+    public void AddDryIce()
+    {
+        DBofBox temp = gameObject.AddComponent<DBofBox>();
+        temp.AWBoption = new string[]
+        {
+            "Missing the \"Net Quantity weight of Dry Ice\"",
+            "Incorrext Hazard Label",
+            "Incorrect placement of the \"shipper and consignee Mark\"",
+            "Missing the \"Proper Shipping Name",
+            "Missing the word\"UN\"",
+            "Incorrect \"UN number\""
+        };
+        temp.Packageoption = new string[]
+        {
+            "Incorrect usage of \"DG statement\" / Need to be removed",
+            "Missing the word\"UN\"",
+            "Missing the \"Net Quantity of Dry Ice\"",
+            "Missing the \"Proper Shipping Name\"",
+            "Incorrect \"UN number\""
+        };
+        temp.Dgdoption = new string[]
+        {
+            "Non, there is no DGD"
+        };
+    }
+}
