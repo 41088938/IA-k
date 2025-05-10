b019fe98b92414e343ab3afa63359c6fc2cc6ebc
tattattat34 <162422438+tattattat34@users.noreply.github.com>
2025-05-10 18:43:03 +0800

    1
    1

--- Assets/UI/GameScenceUI/Book/BookControler.cs
+++ Assets/UI/GameScenceUI/Book/BookControler.cs
@@ -25,8 +25,6 @@ public class BookControler : MonoBehaviour
 
         book_animator.Play("Book_In_Animation");
         r3d_controler.enabled = false;
-        Debug.Log("Hi");
-
 
     }
    public void OffBook()
